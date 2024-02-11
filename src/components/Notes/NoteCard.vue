8a64f478a7aa32f2c56e58a8dad8895be6d81f37
Andraz <a.bergant96@gmail.com>
2024-02-11 20:23:10 +0100

    add note

---
+++ src/components/Notes/NoteCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">{{ props.note.content }}</div>
+      <div class="has-text-right has-grey-light mt-2">
+        <small>{{ characterLength }}</small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+// imports
+import { computed } from 'vue'
+// props
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true
+  }
+})
+
+// character length
+const characterLength = computed(() => {
+  let length = props.note.content.length
+  let description = length > 1 ? 'characters' : 'character'
+  return `${length} ${description}`
+})
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,29 +3,58 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add New Note</button>
+          <button
+            class="button is-link has-background-success"
+            :disabled="!newNote"
+            @click="addNote"
+          >
+            Add New Note
+          </button>
         </div>
       </div>
-    </div>
-
-    <div v-for="i in 3" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut
-          ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit,
-          corrupti omnis, eaque animi.
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
+      <NoteCard v-for="note in notes" :key="note.id" :note="note" />
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import NoteCard from '@/components/Notes/Note.vue'
+
+const newNote = ref('')
+const newNoteRef = ref(null)
+const notes = ref([
+  {
+    id: 'id1',
+    content:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint utullam culpa nulla molestia'
+  },
+  {
+    id: 'id2',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. 2'
+  }
+])
+
+const addNote = () => {
+  let id = new Date().getTime().toString()
+  let note = {
+    id,
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+  newNote.value = ''
+  newNoteRef.value.focus()
+}
+</script>
