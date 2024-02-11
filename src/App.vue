89b2c0105a921fa8fa8ab9ee0d622fa907f6fe52
Andraz <a.bergant96@gmail.com>
2024-02-11 18:23:47 +0100

    add notes design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <RouterLink to="/" class="button">Notes</RouterLink> |
-  <RouterLink to="/stats" class="button">Stats</RouterLink>
+  <NavBar></NavBar>
 
-  <RouterView />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+import NavBar from '@/components/Layout/NavBar.vue'
+</script>
 
 <style>
 @import 'bulma/css/bulma.min.css';
